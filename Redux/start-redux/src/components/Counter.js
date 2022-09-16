db59b5910cde167fb4702e9f652b13b5a3090326
lIIIlIIIlIIIl <sdhsdj6450@gmail.com>
2022-09-16 18:09:59 +0900

    createSlice

--- Redux/start-redux/src/components/Counter.js
+++ Redux/start-redux/src/components/Counter.js
@@ -1,25 +1,29 @@
 import { useSelector, useDispatch } from 'react-redux'; // connect : 클래스 컴포넌트 근처에서 클래스 컴포넌트를 스토어에 연결
+import { counterActions } from '../store/index';
+
 import classes from './Counter.module.css';
+
 
 const Counter = () => {
   const dispatch = useDispatch(); // Redux store에 대한 action을 보냄
-  const counter = useSelector(state => state.counter); // store가 관리하는 데이터에 접근하는 방법
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter); // store가 관리하는 데이터에 접근하는 방법
+  const show = useSelector(state => state.counter.showCounter);
 
+  console.log(counter.counter)
   const incrementHandler = () => {
-    dispatch({type: 'increment'});
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({type: 'increase', amount: 5});
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({type: 'toggle'});
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- Redux/start-redux/src/store/index.js
+++ Redux/start-redux/src/store/index.js
@@ -5,7 +5,7 @@ const initialState = {counter:0, showCounter: true}
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState : initialState,
+    initialState,
     reducers:{
         increment(state){
             state.counter++;
@@ -14,7 +14,7 @@ const counterSlice = createSlice({
             state.counter--;
         },
         increase(state, action){
-            state.counter = state.counter + action.amount;
+            state.counter = state.counter + action.payload;
         },
         toggleCounter(state){
             state.showCounter = !state.showCounter;
@@ -59,7 +59,13 @@ const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
     },
-}); // configureStore은 createStore처럼 store를 만든다. 다른점은 여러 개의 리듀서를 하나의 리듀서로 쉽게 합칠 수 있다.
+}); 
+// configureStore은 createStore처럼 store를 만든다. 다른점은 여러 개의 리듀서를 하나의 리듀서로 쉽게 합칠 수 있다.
+
+// createSlice는 서로 다른 리듀서에 해당하는 고유 액션 식별자를 자동으로 생성한다. 
+// counterSlice.actions를 사용하면 액션 식별자 값을 얻을 수 있다.
+// counterSlice.actions에는 작성된 함수의 리듀서 영역에 있는 key의 이름이 들어있어 해당 메서드에 접근 할 수 있다.
+export const counterActions = counterSlice.actions;
 
 export default store;
 
